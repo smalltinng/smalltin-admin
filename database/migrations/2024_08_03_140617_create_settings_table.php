a9679d838f927660d3114070d507aed0a0993053
unknown <hybiekay2@gmail.com>
2024-08-03 16:52:43 +0100

    Hero 212

--- database/migrations/2024_08_03_140617_create_settings_table.php
+++ database/migrations/2024_08_03_140617_create_settings_table.php
@@ -16,8 +16,11 @@ return new class extends Migration
         Schema::create('settings', function (Blueprint $table) {
             $table->id();
             $table->integer("total_users")->default(0);
+            $table->integer("total_winners")->default(0);
+            $table->integer("monthly_winnings")->default(0);
             $table->float("total_amount")->default(0);
-            $table->float("approstimate_pay_amount")->default(0);
+            $table->float("approximate_pay_amount")->default(0);
+
             $table->timestamps();
         });
     }
